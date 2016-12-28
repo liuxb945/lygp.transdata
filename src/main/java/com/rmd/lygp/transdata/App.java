1120efd3ca3a4a2fd47a0195135fc192751abcf9
smth <smth@smth-PC>
2016-12-28 19:24:42 +0800

    dd

---
+++ src/main/java/com/rmd/lygp/transdata/App.java
@@ -0,0 +1,13 @@
+package com.rmd.lygp.transdata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
